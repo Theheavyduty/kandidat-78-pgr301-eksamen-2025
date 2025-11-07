260b19da58b0d826b030c31332cd7d86a0f75986
Theheavyduty <luongalex7@gmail.com>
2025-11-07 14:17:26 +0100

    Changed region secret

--- infra-s3/providers.tf
+++ infra-s3/providers.tf
@@ -1,28 +1,28 @@
 terraform {
-required_version = ">= 1.6.6"
+  required_version = ">= 1.6.6"
 
 
-required_providers {
-aws = {
-source = "hashicorp/aws"
-version = ">= 5.0"
-}
-}
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
+    }
+  }
 
 
-# Terraform state i S3 (krav)
+  # Terraform state i S3 (krav)
 
-#test
-backend "s3" {
-bucket = "pgr301-terraform-state"
-key = "kandidat-<KANDIDATNR>/infra-s3/terraform.tfstate" # Overstyres i CI
-region = "eu-west-1"
-encrypt = true
-# dynamodb_table = "pgr301-terraform-lock" # valgfritt hvis tabellen finnes
-}
+  #test
+  backend "s3" {
+    bucket  = "pgr301-terraform-state"
+    key     = "kandidat-<KANDIDATNR>/infra-s3/terraform.tfstate" # Overstyres i CI
+    region  = "eu-west-1"
+    encrypt = true
+    # dynamodb_table = "pgr301-terraform-lock" # valgfritt hvis tabellen finnes
+  }
 }
 
 
 provider "aws" {
-region = var.aws_region
+  region = var.aws_region
 }
